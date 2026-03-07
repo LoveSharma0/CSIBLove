e147e296a5f6d71ba908f6047140d92ee760985e
LoveSharma0 <lovesharma9733@gmail.com>
2026-03-07 16:07:42 +0530

    Milestone 4 completed - User registration with bcrypt and MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend server is running");
-});
+// MongoDB Atlas connection
+mongoose.connect("mongodb://lovesharma9733_db_user:Love112009@ac-bg3v3fm-shard-00-00.bf4xkqw.mongodb.net:27017,ac-bg3v3fm-shard-00-01.bf4xkqw.mongodb.net:27017,ac-bg3v3fm-shard-00-02.bf4xkqw.mongodb.net:27017/?ssl=true&replicaSet=atlas-xhsrme-shard-0&authSource=admin&appName=Cluster0")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// routes
+app.use("/api/auth", authRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
 });
